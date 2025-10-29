3364b6615753cebf347fbb2abc94b14518c40df8
pilipandr770 <pilipandr770@users.noreply.github.com>
2025-10-29 14:20:59 +0100

    Fix mailguard blueprint template_folder path

--- app/mailguard/__init__.py
+++ app/mailguard/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
 
-mailguard_bp = Blueprint('mailguard', __name__, url_prefix='/mailguard')
+mailguard_bp = Blueprint(
+    'mailguard', 
+    __name__, 
+    url_prefix='/mailguard',
+    template_folder='templates'
+)
 
 from . import views, oauth
