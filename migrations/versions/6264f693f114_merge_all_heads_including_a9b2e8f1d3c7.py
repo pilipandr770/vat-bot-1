46813a8d12006b2227e178e3a038ff26e0c10dc1
pilipandr770 <pilipandr770@users.noreply.github.com>
2026-01-30 18:44:20 +0100

    Add final merge migration (6264f693f114) to resolve all heads including a9b2e8f1d3c7

---
+++ migrations/versions/6264f693f114_merge_all_heads_including_a9b2e8f1d3c7.py
@@ -0,0 +1,24 @@
+"""merge all heads including a9b2e8f1d3c7
+
+Revision ID: 6264f693f114
+Revises: 51d3555188cc, a9b2e8f1d3c7
+Create Date: 2026-01-30 18:43:43.111426
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6264f693f114'
+down_revision = ('51d3555188cc', 'a9b2e8f1d3c7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
