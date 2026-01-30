d38d75e2012841f5f7fefad9e07b93cf8dc02b23
pilipandr770 <pilipandr770@users.noreply.github.com>
2026-01-30 18:26:00 +0100

    Fix: Merge multiple migration heads (3ead14968157 + 7b1be3569a24)

---
+++ migrations/versions/51d3555188cc_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""merge multiple heads
+
+Revision ID: 51d3555188cc
+Revises: 3ead14968157
+Create Date: 2026-01-30 18:25:11.067613
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51d3555188cc'
+down_revision = ('3ead14968157', '7b1be3569a24')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
